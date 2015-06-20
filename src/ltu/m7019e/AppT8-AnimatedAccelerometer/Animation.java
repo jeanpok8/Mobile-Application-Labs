4aa9676b12c4a0bbbc66c4d4b997c59ad7f6b4fe
BAMBANZA Jean Paul <jeanpok8@gmail.com>
2015-06-20 18:00:47 +0200

    Create Animation.java

---
+++ src/ltu/m7019e/AppT8-AnimatedAccelerometer/Animation.java
@@ -0,0 +1,18 @@
+package com.example.accelerometer;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Animation extends Activity {
+    private AnimView view;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        view = new AnimView(this);
+        setContentView(view);
+    }
+}
